5ec0d515a14b3d3f99991db0685cf3e2740801ba
LizulDev <luizfelipekauedasilva@gmail.com>
2024-01-07 16:49:57 -0300

    Parte escrita do projeto em README e desenvolvimento de desafio

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# desafio_javascript
+# Desafio_Javascript 💻💻
+
+## - Criar uma **variável** para armazenar o nome e a quantidade de experiência XP de um heroi, depois utilizar uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+### Se XP for menor que 1000 = Ferro
+### Se XP for entre 1001 e 2000  = Bronze
+### Se XP for entre 2001 e 5000 = Prata 
+### Se XP for entre 6001 e 7000 = Ouro
+### Se XP for entre 5001 e 8000 = Platina 
+### Se XP for entre 8001 e 9000 = Ascendente
+### Se XP for entre 9001 e 10000 = Imortal
+### Se XP for maior ou igual a 10001 = Radiante
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 // Desafio pela DIO para Javascript usando o motor Node JS;
-function somaValores(a, b){
-    return a + b
+//Made by Luiz Felipe
+//07/01/2024
+
+let characterName = "Darkline"
+let xpQuantities = [900, 1500, 3150, 5160, 7900, 8776, 9200, 15000, 2000000]
+let nivel;
+let counter = 0
+
+for (counter = 0; counter < 9; counter++){
+    if (xpQuantities[counter] < 1000){
+        nivel = "Ferro"
+    }
+    else if(xpQuantities[counter] > 1000 && xpQuantities[counter] <= 2000 ){
+        nivel = "Bronze"
+    }
+    else if(xpQuantities[counter] > 2000 && xpQuantities[counter] <= 5000 ){
+        nivel = "Prata"
+    }
+    else if(xpQuantities[counter] > 5000 && xpQuantities[counter] <= 7000 ){
+        nivel = "Ouro"
+    }
+    else if(xpQuantities[counter] > 7000 && xpQuantities[counter] <= 8000 ){
+        nivel = "Platina"
+    }
+    else if(xpQuantities[counter] > 8000 && xpQuantities[counter]<= 9000 ){
+        nivel = "Ascendente"
+    }
+    else if(xpQuantities[counter] > 9000 && xpQuantities[counter] <= 10000 ){
+        nivel = "Imortal"
+    }
+    else if(xpQuantities[counter] > 10000 && xpQuantities[counter] <= 1000000){
+        nivel = "Radiante"
+    }
+    else if (xpQuantities[counter] > 1000000){
+        nivel = "Grão Mestre Felipão"
+    }
+    console.log("============================================================")
+    console.log("O herói de nome " + characterName + " está no nivel " + nivel)
+    
 }
-console.log("Iniciando o programa...")
-console.log("Escrevendo meu nome novamente")
 
-let name = "Luiz Felipe"
-
-console.log("Meu nome é " + name)
-
-
-
+console.log("\n")
 
