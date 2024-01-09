b7a25919fe9f969a91701cb3906d545a1d08b9d3
Luiz Felipe Kauê da Silva <130705905+LizulDev@users.noreply.github.com>
2024-01-09 12:56:36 -0300

    docs: Update index.js

--- index.js
+++ index.js
@@ -4,39 +4,39 @@
 
 let characterName = "Darkline"
 let xpQuantities = [900, 1500, 3150, 5160, 7900, 8776, 9200, 15000, 2000000]
-let nivel;
+let level;
 let counter = 0
 
 for (counter = 0; counter < 9; counter++){
     if (xpQuantities[counter] < 1000){
-        nivel = "Ferro"
+        level = "Ferro"
     }
     else if(xpQuantities[counter] > 1000 && xpQuantities[counter] <= 2000 ){
-        nivel = "Bronze"
+        level = "Bronze"
     }
     else if(xpQuantities[counter] > 2000 && xpQuantities[counter] <= 5000 ){
-        nivel = "Prata"
+        level = "Prata"
     }
     else if(xpQuantities[counter] > 5000 && xpQuantities[counter] <= 7000 ){
-        nivel = "Ouro"
+        level = "Ouro"
     }
     else if(xpQuantities[counter] > 7000 && xpQuantities[counter] <= 8000 ){
-        nivel = "Platina"
+        level = "Platina"
     }
     else if(xpQuantities[counter] > 8000 && xpQuantities[counter]<= 9000 ){
-        nivel = "Ascendente"
+        level = "Ascendente"
     }
     else if(xpQuantities[counter] > 9000 && xpQuantities[counter] <= 10000 ){
-        nivel = "Imortal"
+        level = "Imortal"
     }
     else if(xpQuantities[counter] > 10000 && xpQuantities[counter] <= 1000000){
-        nivel = "Radiante"
+        level = "Radiante"
     }
     else if (xpQuantities[counter] > 1000000){
-        nivel = "Grão Mestre Felipão"
+        level = "Grão Mestre Felipão"
     }
     console.log("============================================================")
-    console.log("O herói de nome " + characterName + " está no nivel " + nivel)
+    console.log("O herói de nome " + characterName + " está no nivel " + level)
     
 }
 
